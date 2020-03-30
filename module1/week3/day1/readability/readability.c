1c83a7df5129c2b5e613a2e1457306691af8bf52
Josue Narro <josue.narro.a@gmail.com>
2020-03-30 17:53:26 -0500

    Add readability

---
+++ module1/week3/day1/readability/readability.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    string text = get_string("Enter some text:\n");
+
+    // printf("Text: %s\n", text);
+
+    // to count letters
+    int letters = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122))
+        {
+            letters++;
+        }
+    }
+
+    // to count words
+    int words = 1;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == 32)
+        {
+            words++;
+        }
+    }
+
+    // to count sentences
+    int sentences = 0;
+
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if (text[i] == 46 || text[i] == 33 || text[i] == 63)
+        {
+            sentences++;
+        }
+    }
+
+    // Coleman-Liau index
+
+    float L = (float)letters / (float)words * 100;
+    float S = (float)sentences / (float)words * 100;
+
+    float index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 1 && index <= 16)
+    {
+        printf("Grade %i\n", (int)index);
+    }
+    else
+    {
+        printf("Grade 16+\n");
+    }
+}
